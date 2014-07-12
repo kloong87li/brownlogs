8b72fca31fd8a33456a09eb0f60b9cad04fa7333
Jonathan Poon <jonathanysp@gmail.com>
2014-07-12 03:00:36 -0700

    add intent

--- www/js/index.js
+++ www/js/index.js
@@ -16,10 +16,10 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+var app2 = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+       this.bindEvents();
     },
     // Bind Event Listeners
     //
@@ -33,17 +33,23 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        app2.receivedEvent('deviceready');
+
+		window.plugins.webintent.getUri(function(url) {
+			if (url != undefined) {
+                window.location.hash = "#/stall/80";
+			}
+		});
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        // var parentElement = document.getElementById(id);
+        // var listeningElement = parentElement.querySelector('.listening');
+        // var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        // console.log('Received Event: ' + id);
     }
 };
