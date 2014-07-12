1a262a8fdcbafba7a8b092fa90e24fa138b24dd5
Jeremy Kao <jermsthekao@gmail.com>
2014-07-11 21:18:22 -0700

    added angular route on /

---
+++ www/js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('brownLogApp', ['ngRoute']);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: '/www/partials/test.html',
+        controller: 'MainController'
+      });
+  }]);
+
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }]);
+
+angular.element(document).ready(function() {
+      angular.bootstrap(document, ['brownLogApp']);
+  });
--- www/js/controllers/MainController.js
+++ www/js/controllers/MainController.js
@@ -1,9 +1,7 @@
-var app = angular.module('brownLogApp', []);
-
 app.controller('MainController', ['$scope', function($scope) {
       $scope.helloWorld = 'Hello World!';
   }]);
 
-angular.element(document).ready(function() {
-      angular.bootstrap(document, ['brownLogApp']);
-	});
+
+
+
