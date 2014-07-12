bb7cc6a48493e8250813ce312bfa2b924b60fae0
kloong87li <kenneth.li87@gmail.com>
2014-07-12 08:30:45 -0700

    made filtering of stalls work

--- www/js/controllers/StallsController.js
+++ www/js/controllers/StallsController.js
@@ -1,10 +1,28 @@
 app.controller('StallsController', ['$scope', '$window', 'StallsResource',
 function($scope, $window, StallsResource) {
+  $scope.filter = "all";
+
+  $scope.setFilter = function(filter) {
+    $scope.filter = filter;
+    filterStalls();
+  }
 
   StallsResource.fetchStalls({}, function(response){
-    console.log(response);
-    $scope.stalls = response.stalls;
+    $scope.allStalls = response.stalls;
+    filterStalls();
   });
+
+  function filterStalls() {
+    switch ($scope.filter) {
+      case 'all':
+        $scope.stalls = $scope.allStalls;
+        break;
+      default:
+        $scope.stalls = $scope.allStalls.filter(function(el){
+          return el.gender === $scope.filter;
+        })
+    }
+  }
 
   $scope.goToStall = function(stall) {
     $window.location.href = "#/stall/" + stall.id;
